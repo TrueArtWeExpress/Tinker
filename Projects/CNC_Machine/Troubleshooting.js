75ddf573f69d70dce24d3969ea59987353fffbaa
Hendrickx Pieter <Hendrickxpieter1986@gmail.com>
2019-07-11 20:52:21 +0200

    Added troubleshooting file

---
+++ Projects/CNC_Machine/Troubleshooting.js
@@ -0,0 +1,8 @@
+// PROBLEM : to much margin on Z-Axis
+// SOLUTION_TOTEST : Add extra bearing on Axis
+
+// PROBLEM : Fixation Dremel pen is weak
+// SOLUTION_TOTEST : Redesign pen holder
+
+// PROBLEM : Motor Z-Axis not enough fixated
+// SOLUTION_TOTEST : Design motor holder
